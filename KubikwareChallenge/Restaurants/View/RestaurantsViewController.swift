da180832f24d8fa30695dc285727ea83056f1749
Marco Siracusano <marc.siracusano@gmail.com>
2023-02-12 16:48:50 -0300

    Initial programmatic setup.

--- KubikwareChallenge/Restaurants/View/RestaurantsViewController.swift
+++ KubikwareChallenge/Restaurants/View/RestaurantsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RestaurantsViewController.swift
 //  KubikwareChallenge
 //
 //  Created by Marco Siracusano on 12/02/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
-
-
 }
 
